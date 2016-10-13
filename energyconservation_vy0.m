e465a4d9a65741e1ba4b6746fde04251e2f5f1d1
Daniel Cox <danielcox16@hotmail.com>
2016-10-13 21:33:50 +0200

    commented energyconservation_vy0

--- energyconservation_vy0.m
+++ energyconservation_vy0.m
@@ -1,22 +1,26 @@
+% Normalized Standard Deviation of Total Energy as a function of initial node velocity
+% Runs simulation for different initial node velocities
 clear; close all; clc;
 
-% Define what dt's to test
-vy0_min = 1e-3;
-vy0_max = 2e0;
-vy0_step = 1e-2;
-settings.steps = 30000;
-settings.dt = 1e-3;
+% Define what vy0's to test
+vy0_min = 1e-3;							% Minimum vy0
+vy0_max = 2e0;							% Maximum vy0
+vy0_step = 1e-2;						% vy0 step
+settings.steps = 30000;					% Number of simulation time steps
+settings.dt = 1e-3;						% Custom time step size
 
 % Initalizations
-vy0_range = vy0_min:vy0_step:vy0_max;
-n_sims = length(vy0_range);
-std_Etot = zeros(1,n_sims);
+vy0_range = vy0_min:vy0_step:vy0_max;	% vy0 range
+n_sims = length(vy0_range);				% Get number of simulations to run
+std_Etot = zeros(1,n_sims);				% Initialize norm std of Etot
 
-% Test all the dt's
-for i = 1:n_sims
-	settings.vy0 = vy0_range(i);
-	[~,~,~,~,Etot] = guitarstring(settings);
-	std_Etot(i) = std(Etot)/mean(Etot);
+% Test all the vy0's
+for i = 1:n_sims						% Loop (for different vy0's)
+	settings.vy0 = vy0_range(i);		% Apply vy0 in settings
+	[~,~,~,~,Etot] = guitarstring(settings);	% Run simulation
+	std_Etot(i) = std(Etot)/mean(Etot);	% Calculate norm std of Etot
+	
+	% Show progress in console
 	fprintf('%i/%i - %.0f%% done\n',i,n_sims,round(i/n_sims*100));
 end
 
