564d33360b4953ce6845f982a7ef96367aa7bad9
Daniel Cox <danielcox16@hotmail.com>
2016-10-13 21:27:14 +0200

    commented energyconservation_p
    also updated todo.txt

--- energyconservation_p.m
+++ energyconservation_p.m
@@ -1,22 +1,26 @@
+% Normalized Standard Deviation of Total Energy as a function of picking position
+% Runs simulation for different picking positions
 clear; close all; clc;
 
-% Define what dt's to test
-settings.n = 125;
-p_min = 2;
-p_max = settings.n-1;
-p_step = 1;
-settings.steps = 30000;
+% Define what p's to test
+settings.n = 125;						% Set custom number of nodes
+p_min = 2;								% Minimum picking position
+p_max = settings.n-1;					% Maximum picking position
+p_step = 1;								% Picking position step
+settings.steps = 30000;					% Number of simulation time steps
 
 % Initalizations
-p_range = p_min:p_step:p_max;
-n_sims = length(p_range);
-std_Etot = zeros(1,n_sims);
+p_range = p_min:p_step:p_max;			% Generate picking position range 
+n_sims = length(p_range);				% Get number of simulations
+std_Etot = zeros(1,n_sims);				% Initialize norm std of Etot array
 
-% Test all the dt's
-for i = 1:length(p_range)
-	settings.p = p_range(i);
-	[~,~,~,~,Etot] = guitarstring(settings);
-	std_Etot(i) = std(Etot)/mean(Etot);
+% Test all the p's
+for i = 1:length(p_range)				% Loop over picking positions
+	settings.p = p_range(i);			% Apply picking position in settings
+	[~,~,~,~,Etot] = guitarstring(settings);	% Run simulation
+	std_Etot(i) = std(Etot)/mean(Etot);	% Calculate norm std of Etot
+	
+	% Clear console and show progress
 	clc
 	fprintf('%i/%i - %.0f%% done\n',i,n_sims,round(i/n_sims*100));
 end
