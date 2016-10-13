a44ab69143c4efef66ac1fb327539157f600d818
Daniel Cox <danielcox16@hotmail.com>
2016-10-13 20:46:21 +0200

    update and comment animate script

--- guitarstring_animate.m
+++ guitarstring_animate.m
@@ -1,13 +1,17 @@
-close all;
-settings.dt = 0.01;
-[x,y,vx,vy,Etot] = guitarstring(settings);
-[imax,~] = size(x);
-xani = downsample(x,10);
-yani = downsample(y,10);
+% Script for showing the result as an animation
+clear; close all; clc;
 
-figure
-for i=1:imax
-	plot(xani(i,:),yani(i,:),'.-')
-	ylim([-0.05 0.05])
-	drawnow
+% Simulate and prepare data
+settings.dt = 0.01; 						% Set the time step a little bigger
+[x,y,vx,vy,Etot] = guitarstring(settings);	% Do the simulation
+[imax,~] = size(x);							% fetch the number of frames
+xani = downsample(x,10);					% Downsample x for faster animation
+yani = downsample(y,10);					% Downsample y for faster animation
+
+% Show animation as a series of plots
+figure										% Spawn new figure
+for i=1:imax								% Loop over frames
+	plot(xani(i,:),yani(i,:),'.-')			% Plot the string
+	ylim([-0.08 0.08])						% Fix y limits of plot
+	drawnow									% Force plot drawing priority
 end
