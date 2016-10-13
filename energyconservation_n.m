df8ed0cf35345343acf14381398fd0979e81cb99
Daniel Cox <danielcox16@hotmail.com>
2016-10-13 21:16:28 +0200

    commented energyconservation_n

--- energyconservation_n.m
+++ energyconservation_n.m
@@ -1,21 +1,24 @@
+% Normalized Standard Deviation of Total Energy as a function of number of nodes
+% Runs simulation for different number of nodes
 clear; close all; clc;
 
-% Define what dt's to test
-n_min = 3;
-n_max = 240;
-n_step = 1;
-settings.steps = 30000;
-
+% Define what n's to test
+n_min = 3;								% Minimum number of nodes
+n_max = 240;							% Maximum number of nodes
+n_step = 1;								% Number of nodes step
+settings.steps = 30000;					% Number of simulation time steps
 
 % Initalizations
-n_range = n_min:n_step:n_max;
-std_Etot = zeros(1,length(n_range));
+n_range = n_min:n_step:n_max;			% Generate number of nodes range
+std_Etot = zeros(1,length(n_range));	% Initialize normalized std of Etot array
 
-% Test all the dt's
-for i = 1:length(n_range)
-	settings.n = n_range(i);
-	[~,~,~,~,Etot] = guitarstring(settings);
-	std_Etot(i) = std(Etot)/mean(Etot);
+% Test all the n's
+for i = 1:length(n_range)				% Loop over all numbers of nodes
+	settings.n = n_range(i);			% Apply n in settings
+	[~,~,~,~,Etot] = guitarstring(settings);	% Do simulation
+	std_Etot(i) = std(Etot)/mean(Etot);	% Calculate norm std of Etot
+	
+	% Clear console and show progress
 	clc
 	fprintf('%i/%i - %.0f%% done\n',i,length(n_range),round(i/length(n_range)*100));
 end
