ecca9ce0f8ded1918a6c84780fc10a1ed474e281
Daniel Cox <danielcox16@hotmail.com>
2016-10-13 21:11:27 +0200

    commented energyconservation_dt

--- energyconservation_dt.m
+++ energyconservation_dt.m
@@ -1,20 +1,24 @@
+% Normalized Standard Deviation of Total Energy as a function of time step size
+% Runs simulation for different time steps dt
 clear; close all; clc;
 
 % Define what dt's to test
-dt_min = 1e-4;
-dt_max = 4e-2;
-dt_step = 4e-4;
-settings.steps = 30000;
+dt_min = 1e-4;				            % Set minimum dt value to test
+dt_max = 4e-2;				            % Set maximum dt value to test
+dt_step = 4e-4;				            % Set dt steps
+settings.steps = 30000;		            % Set number of simulation time steps
 
 % Initalizations
-dt_range = dt_min:dt_step:dt_max;
-std_Etot = zeros(1,length(dt_range));
+dt_range = dt_min:dt_step:dt_max;		% Generate dt-range
+std_Etot = zeros(1,length(dt_range));	% Initialize std of total energy array
 
 % Test all the dt's
-for i = 1:length(dt_range)
-	settings.dt = dt_range(i);
-	[~,~,~,~,Etot] = guitarstring(settings);
-	std_Etot(i) = std(Etot)/mean(Etot);
+for i = 1:length(dt_range)				% Loop over dt-range
+	settings.dt = dt_range(i);			% Set dt in settings struct
+	[~,~,~,~,Etot] = guitarstring(settings);	% Do simulation and get Etot
+	std_Etot(i) = std(Etot)/mean(Etot);	% Calculate normalized std of Etot
+	
+	% Print progress to console
 	fprintf('%i/%i - %.0f%% done\n',i,length(dt_range),round(i/length(dt_range)*100));
 end
 
